b205f11ff0b16d8e459e067827af5598286a105e
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2019-12-14 16:06:47 -0300

    Implemented editor boilerplate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useMemo, useState } from 'react';
+import { createEditor } from 'slate';
+import { Slate, Editable, withReact } from 'slate-react';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of a paragraph...'}],
+    },
+  ]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+      <Editable/>
+    </Slate>
   );
-}
+};
 
 export default App;
+
