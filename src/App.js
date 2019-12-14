f5aec668f27af0163f91c30c2244cbf95419b0be
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2019-12-14 16:15:06 -0300

    Save changes in local storage

--- src/App.js
+++ src/App.js
@@ -6,15 +6,27 @@ import './App.css';
 
 const App = () => {
   const editor = useMemo(() => withReact(createEditor()), []);
-  const [value, setValue] = useState([
+  const [value, setValue] = useState(
+    JSON.parse(localStorage.getItem('content')) || [
     {
       type: 'paragraph',
       children: [{ text: 'A line of a paragraph...'}],
     },
   ]);
+
+  const onValueChange = value => {
+    setValue(value);
+    saveInLocalStorage(value);
+  }
+
+  const saveInLocalStorage = value => {
+    const content = JSON.stringify(value);
+    localStorage.setItem('content', content);
+    console.log(content);
+  }
   
   return (
-    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+    <Slate editor={editor} value={value} onChange={onValueChange}>
       <Editable/>
     </Slate>
   );
