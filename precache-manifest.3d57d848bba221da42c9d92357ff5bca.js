aa467950fa526ddef241e072406acc4d3e0eb140
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2019-12-14 16:35:50 -0300

    Updates

---
+++ precache-manifest.3d57d848bba221da42c9d92357ff5bca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d732b0340bad966e01be0a3dcffcf042",
+    "url": "/mio-editor/index.html"
+  },
+  {
+    "revision": "fd90a0d23dceca2509b0",
+    "url": "/mio-editor/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0e5e814e80c63014855a",
+    "url": "/mio-editor/static/js/2.c33068ad.chunk.js"
+  },
+  {
+    "revision": "bccda1498540856f360778dbe884c6ae",
+    "url": "/mio-editor/static/js/2.c33068ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd90a0d23dceca2509b0",
+    "url": "/mio-editor/static/js/main.c2a38600.chunk.js"
+  },
+  {
+    "revision": "911b3cf60d286a6929aa",
+    "url": "/mio-editor/static/js/runtime-main.e2abb17a.js"
+  }
+]);
