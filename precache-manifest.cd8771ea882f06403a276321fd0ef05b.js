50cf86a2391e48d5a9d2f5e7aa60d5f7c0677dac
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2019-12-21 14:34:42 -0300

    Updates

---
+++ precache-manifest.cd8771ea882f06403a276321fd0ef05b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5307ad6fcaa2c5b871c0f494368cc0",
+    "url": "/mio-editor/index.html"
+  },
+  {
+    "revision": "52f343ffbaeaf7da2b1d",
+    "url": "/mio-editor/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f42f7b6763a702b0d6e0",
+    "url": "/mio-editor/static/js/2.fa98f451.chunk.js"
+  },
+  {
+    "revision": "44eea2c4942db6a399d6281ec2dfb48f",
+    "url": "/mio-editor/static/js/2.fa98f451.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52f343ffbaeaf7da2b1d",
+    "url": "/mio-editor/static/js/main.be5bc1f3.chunk.js"
+  },
+  {
+    "revision": "911b3cf60d286a6929aa",
+    "url": "/mio-editor/static/js/runtime-main.e2abb17a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mio-editor/precache-manifest.e353eebd23f4347c3a1f311a45d3710a.js"
+  "/mio-editor/precache-manifest.cd8771ea882f06403a276321fd0ef05b.js"
 );
 
 self.addEventListener('message', (event) => {
