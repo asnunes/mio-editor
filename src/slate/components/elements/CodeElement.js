95bb4fd3b20dca94b4abc551cf6d57a2ffb3124e
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2019-12-27 10:39:27 -0300

    Added code element styles

--- src/slate/components/elements/CodeElement.js
+++ src/slate/components/elements/CodeElement.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 export const CodeElement = ({ attributes, children }) => (
-  <pre>
-    <code style={getCodeDecoration()} {...attributes}>{children}</code>
+  <pre style={getPreDecoration()}>
+    <code {...attributes}>{children}</code>
   </pre>
 );
 
-const getCodeDecoration = () => ({
-  fontFamily: 'monospace',
-  backgroundColor: '#eee',
-  padding: '3px',
+const getPreDecoration = () => ({
+  backgroundColor: '#f4f4f4',
+  border: '1px solid #ddd',
+  borderLeft:  '3px solid black',
+  color: '#666',
+  pageBreakInside: 'avoid',
+  padding: '0 1em',
+  marginBottom: '1.6em',
+  borderRadius: '5px',
+  fontFamily: 'Courier New Courier monospace',
+  overflow: 'auto',
+  wordWrap: 'break-word',
 });
