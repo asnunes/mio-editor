add9597180b4606f55d8a339f7a717cd317a302a
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2019-12-29 11:56:50 -0300

    Implemented basic usage

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import MioEditor from './lib';
-
-const App = () => <MioEditor/>;
-
-export default App;
-
---
+++ src/examples/basicUsage.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import MioEditor from '../lib';
+
+const App = () => {
+  const [value, setValue] = useState(initialValue);
+
+  const onValueChange = value => {
+    setValue(value);
+    console.log(JSON.stringify(value));
+  };
+
+  return (
+    <MioEditor
+      value={value}
+      onValueChange={onValueChange}
+    />
+  );
+};
+
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of a paragraph...'}],
+  },
+];
+
+export default App;
+
