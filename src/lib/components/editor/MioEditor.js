3def56eaf61f794caff880185251e3e6716fe4c2
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2019-12-30 18:00:22 -0300

    Removed external styles files from editor component

--- src/lib/components/editor/MioEditor.js
+++ src/lib/components/editor/MioEditor.js
@@ -9,9 +9,6 @@ import { renderLeaf, renderElement } from '../../renders';
 import { MioHelpers } from '../../helpers';
 import { withAllPlugins } from '../../plugins';
 
-import './stylesheets/reset.css';
-import './stylesheets/style.css';
-
 export const MioEditor = (props) => {
   const editor = useMemo(() => withAllPlugins(withHistory(withReact(createEditor()))), []);
   const [value, setValue] = useState(props.value || initialValue);
@@ -24,7 +21,7 @@ export const MioEditor = (props) => {
   return (
     <Slate editor={editor} value={value} onChange={onValueChange}>
       <Toolbar/>
-      <div className="mio-editor">
+      <div style={style}>
       <MathJax.Context 
         script="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_SVG"
         options={MATH_JAX_OPTIONS}
@@ -59,4 +56,15 @@ const initialValue = [
     children: [{ text: 'A line of a paragraph...'}],
   },
 ];
+
+const style = {
+  width: '21cm',
+  minHeight: '29.7cm',
+  margin: '30mm auto 27mm auto',
+  boxShadow: '0px 2px 5px #888888',
+  borderRadius: '3px',
+  border: '15px 0 auto 0',
+  fontSize: '14pt',
+  padding: '3cm 2cm 2cm 3cm',  
+};
 
