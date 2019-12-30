bbfa1b35be08f643eeb452449365253386d5998e
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2019-12-30 17:15:30 -0300

    Rollup configuration

---
+++ rollup.config.js
@@ -0,0 +1,99 @@
+import babel from 'rollup-plugin-babel';
+import builtins from 'rollup-plugin-node-builtins';
+import commonjs from 'rollup-plugin-commonjs';
+import globals from 'rollup-plugin-node-globals';
+import json from 'rollup-plugin-json';
+import replace from 'rollup-plugin-replace';
+import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+import svg from 'rollup-plugin-svg';
+
+function configure() {
+  const input = 'src/lib/index.js';
+
+  // Stop Rollup from warning about circular dependencies.
+  const onwarn = warning => {
+    if (warning.code !== 'CIRCULAR_DEPENDENCY') {
+      console.warn(`(!) ${warning.message}`) // eslint-disable-line no-console
+    }
+  };
+
+  const plugins = [
+    postcss({
+      extract: false,
+      modules: true,
+      use: ['sass'],
+    }),
+    svg(),
+    resolve({
+      browser: true,
+    }),
+    commonjs({
+      exclude: `src/**`,
+      namedExports: {
+        'react-dom': ['findDOMNode'],
+        'react-dom/server': ['renderToStaticMarkup'],
+        'node_modules/immutable/dist/immutable.js': [
+          'Iterable', 'Seq', 'Collection', 'Map', 'OrderedMap', 'List', 'Stack', 'Set',
+          'OrderedSet', 'Record', 'Range', 'Repeat', 'is', 'fromJS'
+        ],
+        'prop-types': [
+          'array',
+          'bool',
+          'func',
+          'number',
+          'object',
+          'string',
+          'symbol',
+          'any',
+          'arrayOf',
+          'element',
+          'elementType',
+          'instanceOf',
+          'node',
+          'objectOf',
+          'oneOf',
+          'oneOfType',
+          'shape',
+          'exact',
+        ],
+        'node_modules/esrever/esrever.js': [ 'reverse' ]
+      },
+    }),
+    json(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    builtins(),
+    babel({
+      "presets": [
+        ["@babel/preset-env", { modules: false }],
+        "@babel/preset-react"
+      ],
+      exclude: 'node_modules/**',
+    }),
+    globals(),
+    terser(),
+  ].filter(Boolean);
+
+  const globalsLibs = {
+    "react": "React",
+    "react-dom": "ReactDOM",
+  };
+
+  return {
+    plugins,
+    input,
+    onwarn,
+    output: {
+      format: 'umd',
+      file: `dist/mio-editor.min.js`,
+      name: "MioEditor",
+      globals: globalsLibs,
+    },
+    external: Object.keys(globalsLibs),
+  }
+}
+
+export default configure();
