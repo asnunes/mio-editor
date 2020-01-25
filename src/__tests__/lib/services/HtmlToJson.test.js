96c6e2886e69079f4e7eefce70b44c05bbb439b8
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-01-25 16:37:39 -0300

    Implemented paragraph conversion from html to json

--- src/__tests__/lib/services/HtmlToJson.test.js
+++ src/__tests__/lib/services/HtmlToJson.test.js
@@ -1,8 +1,25 @@
 const { HtmlToJson } = require('../../../lib/services/HtmlToJson');
 
-describe('calling function', () => {
-    test('returns Hello, tests!', () => {
-        const result = HtmlToJson('');
-        expect(result).toBe('Hello, tests!');
+describe('#HtmlToJson', () => {
+  describe('pass an html string containing a p tag', () => {
+    test('returns ', () => {
+      const input = `<p>This is a text in a paragraph.</p>`;
+
+      const result = HtmlToJson(input);
+
+      const expected = [
+        {
+          type: "paragraph",
+          children:[
+             {
+                text:"This is a text in a paragraph."
+             },
+          ]
+       },
+      ];
+
+      console.log(JSON.stringify(result));
+      expect(result).toEqual(expected);
     });
+  });
 });
--- src/lib/services/HtmlToJson.js
+++ src/lib/services/HtmlToJson.js
@@ -1,3 +1,23 @@
-export const HtmlToJson = element => {
-    return "Hello, tests!";
+import { jsx } from 'slate-hyperscript';
+
+export const HtmlToJson = html => {
+  const document = new DOMParser().parseFromString(html, 'text/html');
+
+  return deserialize(document.body);
+};
+
+const deserialize = element => {
+  if (element.nodeType === 3) return element.textContent;
+  else if (element.nodeType !== 1) return null; //check node type 
+
+  const children = Array.from(element.childNodes).map(deserialize);
+
+  switch (element.nodeName) {
+    case 'BODY':
+      return jsx('fragment', {}, children);
+    case 'P':
+      return jsx('element', { type: 'paragraph' }, children);
+    default:
+      return el.textContent;
+  }
 };
